cca85cbcc24d9397cc885488e7b4ce339f4cd987
vcombey <vincent.combey@gmail.com>
2025-08-06 06:39:20 +0200

    add better theme

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,5 +1,113 @@
 -- Color themes and appearance
+--
+-- 🎨 HOW TO SWITCH THEMES:
+-- The active colorscheme is set in the catppuccin plugin config below
+-- To switch themes, move the `vim.cmd.colorscheme` line to your preferred theme's config function
+--
+-- Available themes:
+-- - 'catppuccin-macchiato' (currently active)
+-- - 'cyberdream'
+-- - 'synthwave84'
+-- - 'material-palenight'
+-- - 'material-deep-ocean'
+--
 return {
+  { -- Modern cyberpunk theme with neon aesthetics
+    -- cyberdream.nvim provides a beautiful cyberpunk/synthwave aesthetic
+    -- with excellent contrast and readability for long coding sessions
+    'scottmckendry/cyberdream.nvim',
+    priority = 1000,
+    lazy = false,
+
+    opts = {
+      -- Make the theme transparent to show terminal background
+      transparent = false,
+
+      -- Add borders around floating windows for better definition
+      borderless_telescope = false,
+
+      -- Terminal colors match the theme
+      terminal_colors = true,
+    },
+
+    config = function(_, opts)
+      require('cyberdream').setup(opts)
+      -- Cyberdream is no longer the active theme
+      -- vim.cmd.colorscheme 'cyberdream'
+    end,
+  },
+
+  { -- Alternative retro synthwave cyberpunk theme
+    -- Synthwave84 brings that authentic 80s cyberpunk aesthetic
+    'artanikin/vim-synthwave84',
+    priority = 999,
+    lazy = false,
+  },
+
+  { -- Matrix-inspired cyberpunk theme
+    -- Material theme with a dark, cyberpunk-inspired palenight variant
+    'marko-cerovac/material.nvim',
+    priority = 998,
+    lazy = false,
+
+    opts = {
+      contrast = {
+        terminal = false,
+        sidebars = true,
+        floating_windows = true,
+        cursor_line = false,
+        non_current_windows = false,
+        filetypes = {},
+      },
+
+      styles = {
+        comments = { italic = true },
+        strings = {},
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        operators = {},
+        types = {},
+      },
+
+      plugins = {
+        'dap',
+        'gitsigns',
+        'lsp',
+        'mini',
+        'neo-tree',
+        'nvim-cmp',
+        'nvim-notify',
+        'nvim-web-devicons',
+        'telescope',
+        'trouble',
+        'which-key',
+      },
+
+      disable = {
+        colored_cursor = false,
+        borders = false,
+        background = false,
+        term_colors = false,
+        eob_lines = false,
+      },
+
+      high_visibility = {
+        lighter = false,
+        darker = true,
+      },
+
+      lualine_style = 'default',
+      async_loading = true,
+    },
+
+    config = function(_, opts)
+      require('material').setup(opts)
+      -- Available variants: material, material-darker, material-lighter,
+      -- material-oceanic, material-palenight, material-deep-ocean
+      -- vim.g.material_style = 'palenight'  -- Uncomment to use palenight variant
+    end,
+  },
   { -- Alternative colorscheme with multiple variants
     -- Catppuccin is a beautiful, warm colorscheme with excellent plugin integration
     -- It comes in 4 flavors: Latte (light), Frappe, Macchiato, and Mocha (dark)
@@ -11,7 +119,7 @@ return {
 
     opts = {
       -- Choose the flavor/variant of Catppuccin
-      flavour = 'mocha', -- Options: latte, frappe, macchiato, mocha
+      flavour = 'macchiato', -- Options: latte, frappe, macchiato, mocha
 
       -- Automatic light/dark mode switching based on system preference
       background = { light = 'latte', dark = 'mocha' },
@@ -55,6 +163,12 @@ return {
         which_key = true, -- Keymap helper theming
       },
     },
+
+    config = function(_, opts)
+      require('catppuccin').setup(opts)
+      -- Set catppuccin-macchiato as the active colorscheme
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+    end,
   },
 
   { -- You can easily change to a different colorscheme.
@@ -72,10 +186,9 @@ return {
         },
       }
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      -- Cyberdream is set as the active theme in its config above
+      -- To switch to tokyonight, move the colorscheme line to this config function
+      -- Available variants: 'tokyonight-storm', 'tokyonight-moon', 'tokyonight-day'
     end,
   },
 }
